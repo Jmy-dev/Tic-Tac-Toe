550813119ba0ec478eb068ce8a76e1012c047d92
Jmy-dev <61315548+Jmy-dev@users.noreply.github.com>
2020-06-20 07:16:39 +0200

    refactor the code

--- index.js
+++ index.js
@@ -1,148 +1,136 @@
-var td          = document.querySelectorAll("td");
-var resetButton = document.getElementById("reset");
-var table       = document.querySelector("table");
-var playButton  = document.getElementById("play");
-var player      = document.querySelector(".player");
-var ideaimg     = document.querySelector(".idea");
-var winimg      = document.querySelector(".win");
-var drawimg     = document.querySelector(".draw");
-var finish = true;
+var boxes         = document.querySelectorAll("td");
+var resetButton   = document.getElementById("reset");
+var table         = document.querySelector("table");
+var playButton    = document.getElementById("play");
+var player        = document.querySelector(".player");
+var coverImage    = document.querySelector(".cover");
+var winImage      = document.querySelector(".win");
+var drawImage     = document.querySelector(".draw");
+var header        = document.querySelector(".header");
+var secondHeader  = document.querySelector(".header2");
+var finish  = true;
 var counter = 0;
 var X  = [];
 var O  = [];
-var header      = document.querySelector(".header");
-var header2      = document.querySelector(".header2");
-var row         = document.querySelector(".rows");
-var blocks = row.children;
+
 //Start
-const play =() =>{
-  header2.classList.add("hide");
-  header.classList.remove("hide");
-  winimg.classList.add("hide");
+playButton.addEventListener("click" , startGame)
+boxes.forEach(function addListener(box){
+  return box.addEventListener("click" ,handleBoxClicked);
+})
+//reset
+resetButton.addEventListener("click",reset);
+
+//------------------------------------------------------
+function startGame(){
+ switchHeaders(1);
+  winImage.classList.add("hide");
   table.classList.remove("hide");
   resetButton.classList.remove("hide");
   table.classList.add("animate__bounceInDown")
   finish = false;
   playButton.classList.add("hide");
-
-}
-playButton.addEventListener("click" , play)
-
-// game
-for(var i = 0 ; i< td.length ; i++){
-  td[i].addEventListener("click" ,gameCore)
 }
 
-function gameCore(){
-  if(!finish){
-  if(this.textContent == ""){
-    if(player.textContent == "X"){
-      this.textContent = "X" ;
-      player.textContent = "O";
-      X.push(this.attributes.class.nodeValue);
-    }
-    else if(player.textContent == "O"){
-      this.textContent = "O";
-      player.textContent = "X";
-      O.push(this.attributes.class.nodeValue);
-    }
-    counter++;
-    checkWinner(X);
-    checkWinner(O);
-    checkForDraw();
-
-    // console.log(counter);
-
-  }
-}
-}
-
-
-// reset
-resetButton.addEventListener("click",reset);
 function reset(){
   // clear all the td
-  for(let i = 0 ; i< td.length; i++){
-    td[i].textContent = "";
-  }
+  boxes.forEach(clearBoxesContent);
   player.textContent = "X";
   finish = false;
   X = [];
   O = [];
   counter = 0;
-  header2.classList.add('hide');
-  header.classList.remove('hide');
-        winimg.classList.add("hide");
-        drawimg.classList.add("hide");
-        ideaimg.classList.remove("hide");
-
+  switchHeaders(1);
+  winImage.classList.add("hide");
+  drawImage.classList.add("hide");
+  coverImage.classList.remove("hide");
 }
 
-function winner(){
- header.classList.add('hide');
- header2.classList.remove('hide');
- header2.textContent = `${winnner} Wins!!`;
+function handleBoxClicked(){
+   if(!finish){
+     if(this.textContent == ""){
+        if(player.textContent == "X"){
+            this.textContent = "X" ;
+            player.textContent = "O";
+            X.push(this.attributes.class.nodeValue);
+        }
+        else if(player.textContent == "O"){
+            this.textContent = "O";
+            player.textContent = "X";
+            O.push(this.attributes.class.nodeValue);
+        }
+        counter++;
+        checkWining(X);
+        checkWining(O);
+        checkForDraw();
+     }
+   }
+}
+function handleWining(){
+ switchHeaders(2)
+ secondHeader.textContent = `${winnner} Wins!!`;
  finish = true;
-      winimg.classList.remove("hide");
-      ideaimg.classList.add("hide");
+ winImage.classList.remove("hide");
+ coverImage.classList.add("hide");
+}
 
+function handleDraw(){
+  switchHeaders(2);
+  secondHeader.textContent = `Ohhh NoBody Wins!!`;
+  finish = true;
+  coverImage.classList.add("hide");
+  drawImage.classList.remove("hide");
 }
 
 function checkForDraw(){
-  function draw(){
-    header.classList.add('hide');
-    header2.classList.remove('hide');
-    header2.textContent = `Ohhh NoBody Wins!!`;
-  finish = true;
-     ideaimg.classList.add("hide");
-     drawimg.classList.remove("hide");
-
-
-  }
   if(!finish  && counter ===9){
-    draw();
+    handleDraw();
+  }
+}
+function clearBoxesContent(box){
+  box.textContent = "";
+}
+function switchHeaders(state){
+  if(state == 1){
+    secondHeader.classList.add("hide");
+    header.classList.remove("hide");
+  }else if(state ==2){
+    header.classList.add('hide');
+    secondHeader.classList.remove('hide');
   }
 }
 
-function checkWinner(arr){
+function checkWining(arr){
  if( arr.includes("1") && arr.includes("vertical 2")&& arr.includes("3")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("1") && arr.includes("horizontal 4")&& arr.includes("7")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2" ;
+   handleWining();
  }
  else if( arr.includes("7") && arr.includes("vertical 8")&& arr.includes("9")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("horizontal 4") && arr.includes("vertical horizontal 5")&& arr.includes("horizontal 6")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("vertical 2") && arr.includes("vertical horizontal 5")&& arr.includes("vertical 8")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("3") && arr.includes("horizontal 6")&& arr.includes("9")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("1") && arr.includes("vertical horizontal 5")&& arr.includes("9")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+   arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
  else if( arr.includes("3") && arr.includes("vertical horizontal 5")&& arr.includes("7")){
-   if(arr === X){ winnner = "Player1" }
-   else{ winnner = "Player2" }
-   winner();
+    arr == X ? winnner ="Player1" : winnner ="Player2"
+   handleWining();
  }
 }
